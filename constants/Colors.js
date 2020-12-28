0d5fa562158dd6c4f5c0ae75a1007973dc19ac8a
Andy <aste.rix@icloud.com>
2020-12-28 11:57:04 +0100

    adds variables

---
+++ constants/Colors.js
@@ -0,0 +1,15 @@
+export default {
+	backgroundDark: '#132743',
+	primary: '#DB1D4B',
+	secondary: '#FF6187',
+	accent: '#FFC107',
+	accentFade: '#F9E3A2',
+	warn: '#FA8500',
+	shadesGray80: '#333333',
+	shadesGray60: '#555555',
+	shadesGray40: '#777777',
+	shadesGray20: '#999999',
+	nfRed: '#db0000',
+	nfBlack: '#000',
+	nfWhite: '#efefef'
+};
