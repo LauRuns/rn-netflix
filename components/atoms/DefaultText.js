3b1429cca55132ef2f3162cbbe12100e0c71d9bc
Andy <aste.rix@icloud.com>
2020-12-28 21:15:08 +0100

    add default text wrapper

---
+++ components/atoms/DefaultText.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import Colors from '../../constants/Colors';
+
+export const DefaultText = (props) => {
+	return (
+		<Text
+			style={{
+				...styles.text,
+				...{ color: props.color || Colors.nfWhite, fontSize: props.size }
+			}}
+		>
+			{props.children}
+		</Text>
+	);
+};
+
+const styles = StyleSheet.create({
+	text: {
+		fontFamily: 'roboto-regular'
+	}
+});
