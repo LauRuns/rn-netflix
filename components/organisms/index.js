61406404a6b9c12b1d9e90f1fa3b261926b04ba5
Andy <aste.rix@icloud.com>
2020-12-30 17:16:41 +0100

    adds import

--- components/organisms/index.js
+++ components/organisms/index.js
@@ -2,5 +2,6 @@ import { NewNFContentList } from './NewNFContentList';
 import { ExpNFContentList } from './ExpNFContentList';
 import { Input } from './Input';
 import { CountryList } from './CountryList';
+import { SearchForm } from './SearchForm';
 
-export { NewNFContentList, ExpNFContentList, Input, CountryList };
+export { NewNFContentList, ExpNFContentList, Input, CountryList, SearchForm };
