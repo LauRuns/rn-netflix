28019bc8989b7a8b6c9e85b838dbc9a0d1f16797
Andy <aste.rix@icloud.com>
2020-12-30 13:44:20 +0100

    changes render method

--- components/organisms/ExpNFContentList.js
+++ components/organisms/ExpNFContentList.js
@@ -6,25 +6,17 @@ import { NavButtons, ExpNFItem } from '../molecules/index';
 import { DUMMY_ITEMS } from '../../data/DUMMY_DATA';
 
 export const ExpNFContentList = (props) => {
-	const renderNFItemImage = (itemData) => {
-		console.log('itemData', itemData.item);
-		return <ExpNFItem item={itemData.item} navData={props.navData} />;
-	};
-
 	return (
 		<View style={styles.screen}>
 			<NavButtons onNext={props.onNext} onPrevious={props.onPrevious} />
 			<SafeAreaView style={styles.list}>
 				<FlatList
 					data={props.listData}
-					// data={DUMMY_ITEMS}
 					numColumns={2}
 					keyExtractor={(item, index) => item.netflixid.toString()}
-					// keyExtractor={(item, index) => item.nfid.toString()}
-					renderItem={renderNFItemImage}
-					contentContainerStyle={{
-						paddingBottom: 50
-					}}
+					renderItem={(itemData) => (
+						<ExpNFItem item={itemData.item} navData={props.navData} />
+					)}
 				/>
 			</SafeAreaView>
 		</View>
