7554ea76ca42be19bae28e475b44da10695ee78a
Andy <aste.rix@icloud.com>
2021-01-01 12:00:18 +0100

    adds using route params to searchresults

--- screens/search/SearchResultScreen.js
+++ screens/search/SearchResultScreen.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export const SearchResultScreen = (props) => {
+	const {
+		query,
+		contentType,
+		startyear,
+		endyear,
+		countryId
+	} = props.route.params.queryParams;
+	console.log(query, contentType, startyear, endyear, countryId);
+
 	return (
 		<View style={styles.screen}>
 			<Text>SearchResultScreen works</Text>
+			<Text>
+				Search query: {query} for country ID: {countryId}
+			</Text>
 		</View>
 	);
 };
