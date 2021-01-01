348ee045aeb910402b1f3b0abf9ac294aba4ebbe
Andy <aste.rix@icloud.com>
2021-01-01 17:32:58 +0100

    adds userdetail component

--- components/atoms/IconButton.js
+++ components/atoms/IconButton.js
@@ -72,7 +72,6 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		justifyContent: 'center',
 		alignItems: 'center'
-		// backgroundColor: 'purple'
 	},
 	iconContainer: {
 		padding: 10
