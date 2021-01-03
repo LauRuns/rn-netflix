c8b99a8bc27635e6fe8282174d875648492a8327
Andy <aste.rix@icloud.com>
2021-01-03 16:27:02 +0100

    adds screen component

--- screens/account/avatar/AvatarScreen.js
+++ screens/account/avatar/AvatarScreen.js
@@ -1,10 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Alert, Text } from 'react-native';
+
+import { useContextUser } from '../../../shared/context/user-context';
+import { Header } from '../../../components/atoms/index';
+import { Spinner } from '../../../components/molecules/index';
+import { ImageSelector } from '../../../components/organisms/index';
+import Colors from '../../../constants/Colors';
 
 export const AvatarScreen = (props) => {
+	const {
+		updateUserImg,
+		isUpdating,
+		updatingError,
+		clearError
+	} = useContextUser();
+
+	const updateAvatarHandler = async (newAvatar) => {
+		const splitImage = newAvatar.split('ImagePicker/')[1];
+		const formData = new FormData();
+		formData.append('image', {
+			uri: newAvatar,
+			type: 'image/jpeg',
+			name: splitImage
+		});
+		await updateUserImg(formData);
+		props.navigation.navigate('Account');
+	};
+
+	useEffect(() => {
+		if (updatingError) {
+			Alert.alert('Error', updatingError, [
+				{ text: 'OK', onPress: () => clearError() }
+			]);
+		}
+	}, [updatingError]);
+
+	if (isUpdating) {
+		return (
+			<Spinner
+				spinnerText="Updating your avatar..."
+				spinnerSize="large"
+				spinnerColor={Colors.primary}
+				spinnerTextColor={Colors.primary}
+			/>
+		);
+	}
+
 	return (
 		<View style={styles.screen}>
-			<Text>Avatar screen works</Text>
+			{isUpdating && <Text>loading</Text>}
+			<Header
+				title="Set profile avatar"
+				color={Colors.primary}
+				subHeader="Select or take a photo"
+			/>
+			<ImageSelector onSaveAvatar={updateAvatarHandler} />
 		</View>
 	);
 };
@@ -12,7 +62,7 @@ export const AvatarScreen = (props) => {
 const styles = StyleSheet.create({
 	screen: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
+		backgroundColor: Colors.backgroundDark
 	}
 });
