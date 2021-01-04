cf7df0ca048775cff283c6911cea7a61a30ec456
Andy <aste.rix@icloud.com>
2021-01-04 16:50:24 +0100

    adds color and removes import

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,10 +1,13 @@
 export default {
 	backgroundDark: '#132743',
+	backgroundDark60: '#19456b',
+	backgroundDark20: '#11698e',
 	primary: '#DB1D4B',
 	secondary: '#FF6187',
 	accent: '#FFC107',
 	accentFade: '#F9E3A2',
 	warn: '#FA8500',
+	succes: '#54e346',
 	shadesGray80: '#333333',
 	shadesGray60: '#555555',
 	shadesGray40: '#777777',
