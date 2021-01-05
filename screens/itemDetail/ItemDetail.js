364c3f2868ae52ec78e609b4cf8c96339ac1cf7a
Andy <aste.rix@icloud.com>
2021-01-05 19:18:45 +0100

    adds and remove funcionality for favorite items

--- screens/itemDetail/ItemDetail.js
+++ screens/itemDetail/ItemDetail.js
@@ -1,22 +1,88 @@
-import React, { useCallback, useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import React, { useCallback, useState, useEffect } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	ScrollView,
+	Image,
+	TouchableOpacity,
+	TouchableNativeFeedback,
+	Platform,
+	Alert,
+	ActivityIndicator
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-import { NFHeaderButton, DefaultText } from '../../components/atoms/index';
-import { NFItemDetails } from '../../components/molecules/index';
+import { useFavorites } from '../../shared/context/favorites-context';
+import { DefaultText } from '../../components/atoms/index';
 import Colors from '../../constants/Colors';
 
 export const ItemDetail = (props) => {
+	const {
+		favorites,
+		addToFavoritesHandler,
+		removeFromFavoritesHandler,
+		isLoading,
+		error,
+		clearError
+	} = useFavorites();
 	const [isFavorite, setIsFavorite] = useState(false);
+	const [currentItem, setCurrentItem] = useState(null);
 	const nfItem = props.route.params.item;
+	const { nfid, title, year, synopsis, img, imdbrating } = nfItem;
 
 	const toggleFavoriteHandler = () => {
-		// dispatch(toggleFavorite(mealId));
-		setIsFavorite((prevState) => !prevState);
+		if (!isFavorite) {
+			console.log('Add item to favorites');
+			addToFavoritesHandler({ nfid, title, year, synopsis, img, imdbrating });
+			setIsFavorite((prevState) => !prevState);
+		} else if (isFavorite) {
+			removeFromFavoritesHandler(currentItem._id);
+			setIsFavorite((prevState) => !prevState);
+		}
 	};
+
+	useEffect(() => {
+		let foundItem;
+		if (favorites.length !== 0) {
+			foundItem = favorites.find((item) => +item.netflixid === nfid);
+			console.log('FOUND__?__>', foundItem);
+		}
+		if (foundItem) {
+			console.log(foundItem);
+			setCurrentItem(foundItem);
+			setIsFavorite(true);
+		}
+	}, []);
+
+	useEffect(() => {
+		if (error) {
+			Alert.alert('Error', error.message || 'An unexpected error occurred', [
+				{ text: 'OK' }
+			]);
+		}
+	}, [error]);
+
+	let TouchableCmp = TouchableOpacity;
+	if (Platform.OS === 'android' && Platform.OS >= 21) {
+		TouchableCmp = TouchableNativeFeedback;
+	}
 
 	return (
 		<ScrollView contentContainerStyle={styles.viewContainer}>
+			<View style={styles.favHeart}>
+				{isLoading ? (
+					<ActivityIndicator size="small" color={Colors.primary} />
+				) : (
+					<TouchableCmp onPress={toggleFavoriteHandler}>
+						<Ionicons
+							name={isFavorite ? 'heart-sharp' : 'heart-outline'}
+							size={30}
+							color={Colors.primary}
+						/>
+					</TouchableCmp>
+				)}
+			</View>
 			<Image
 				source={{ uri: nfItem.img }}
 				style={{ width: 300, height: 370 }}
@@ -53,31 +119,16 @@ export const ItemDetail = (props) => {
 };
 
 export const detailScreenOptions = (navData) => {
+	const { title } = navData.route.params.item;
 	return {
-		headerTitle: navData.route.params.item.title,
-		headerRight: () => (
-			<HeaderButtons HeaderButtonComponent={NFHeaderButton}>
-				<Item
-					title="Add as fav"
-					// iconName={isFavorite ? 'heart-sharp' : 'heart-outline'}
-					iconName={
-						Platform.OS === 'android' ? 'heart-outline' : 'heart-outline'
-					}
-					onPress={() => {
-						console.log('Add item to favorites');
-						// toggleFavoriteHandler();
-					}}
-				/>
-			</HeaderButtons>
-		)
+		headerTitle: title
 	};
 };
 
 const styles = StyleSheet.create({
 	viewContainer: {
-		height: '100%',
+		flex: 1,
 		alignItems: 'center',
-		paddingTop: 50,
 		backgroundColor: Colors.backgroundDark
 	},
 	detailInfo: {
@@ -88,5 +139,11 @@ const styles = StyleSheet.create({
 	},
 	story: {
 		paddingVertical: 20
+	},
+	favHeart: {
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+		width: '100%',
+		paddingRight: 15
 	}
 });
